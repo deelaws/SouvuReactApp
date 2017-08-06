095acd53bd8e14c5113a2c85608900370b40d334
Waleed Shahid <deelaws89@gmail.com>
2017-08-05 21:15:50 -0700

    Enhance tab selection

--- index.android.js
+++ index.android.js
@@ -26,6 +26,11 @@ import {
   Modal,
 } from 'react-native-router-flux';
 
+import { Icon,
+        SearchBar,
+        FormLabel,
+        FormInput  } from 'react-native-elements';
+
 import MainView from './main';
 import AddMemoryView from './memory/add_memory';
 import Blink from './memory/blink';
@@ -34,6 +39,8 @@ import SettingsView from './settings/settings_view';
 import HomeView from './memory/home_view';
 import SingleMemoryView from './memory/single_memory_view';
 import FilterMemoryView from './memory/filter_view';
+
+import nav_sytle from './styles/nav_bar_style';
 
 var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
     // add an edit memory scene
@@ -62,20 +69,44 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   tabBarStyle: {
-    backgroundColor: '#fcffb7',
+    backgroundColor: '#659df7',
   },
-  tabBarSelectedItemStyle: {
-    backgroundColor: '#ddd',
+  tabBarSelectedItem: {
+    shadowColor: 'black',
+    opacity:30,
+    elevation:60,
+    // backgroundColor: 'grey',
+    //  borderStyle: "dotted",
+    //  borderRadius:50,
+    //  borderColor:"black",
+    //  shadowOffset: {width: 100, height:20},
+  shadowColor : "green",
+
   },
 });
 
-class TabIcon extends React.Component {
-    render(){
-        return (
-            <Text style={{color: this.props.selected ? 'red' :'black'}}>{this.props.title}</Text>
-        );
-    }
-};
+
+//Create a dedicated class that will manage the tabBar icon
+class TabIcon extends Component {
+  render() {
+    var color = this.props.selected ? 'purple' : '#301c2a';
+
+    return (
+      <View style={{flex:1, flexDirection:'column', alignItems:'center', alignSelf:'center', justifyContent: 'center'}}>
+        {/* <Icon style={{color: color}} name={this.props.iconName || "circle"} size={18}/> */}
+         {this.props.selected ? 
+            (<Icon raised name={this.props.title} size={30}/>) :
+            <Icon  reverse name={this.props.title} size={30}/>
+         
+         
+         }
+{/* 
+        <Icon  name='home' size={30}/>
+        <Text style={{color: color, fontSize: 12}}>{this.props.title}</Text> */}
+      </View>
+    );
+  }
+}
 
    function settingViewButton() {
          // 
@@ -84,58 +115,66 @@ class TabIcon extends React.Component {
 
 const scenes = Actions.create(
   <Scene key="root" hideNavBar={true}>
-      <Scene key="tabbar" tabs tabs={true}  tabBarStyle={styles.tabBarStyle}
-                  activeBackgroundColor='#fcffb7'>
-          <Scene key="tab1"  title="Home" icon={TabIcon} navigationBarStyle={{backgroundColor:'#fcffb7'}} titleStyle={{color:'white'}}>
-            <Scene key="home" 
-                  component={HomeView} 
-                  title="Memory Jar!" 
-                  titleStyle={{color:'black'}}
-                  initial={true}
-                  leftTitle="Settings" 
-                  onLeft={() => alert('Right button')}
-                />
-              
-            <Scene key="setting_page" 
-                component={SettingsView} 
-                title="Settings Tab"
-                hideTabBar={false}
-                titleStyle={{color:'black'}}
+    <Scene key="tabbar" tabs tabs={true} 
+            tabBarSelectedItemStyle={styles.tabBarSelectedItem}  
+            tabBarStyle={styles.tabBarStyle}
+                activeBackgroundColor='#659df7'>
+        <Scene key="tab1"  
+               title="home" 
+               //icon={()=> <Icon  name='home' size={30}/>} 
+               icon={TabIcon}
+               navigationBarStyle={{backgroundColor:'#659df7'}} 
+               titleStyle={{color:'white'}}>
+          <Scene key="home"
+                component={HomeView} 
+                title="Memory Jar!"
+                initial={true}
+                icon={()=> <Icon   name='setting' size={30}/>}
+                leftTitle="Settings" 
+                onLeft={() => alert('Right button')}
               />
-          </Scene>
-          <Scene key="tab4"  title="Your Memories"  icon={TabIcon}>
-            <Scene key="main" 
-              component={MainView} 
-              title="Memory App"
-              onEnter={()=>console.log('onEnter')}
-              onExit={()=>console.log('onExit')}
-              onRight={() => Actions.filter_view()}
-              rightTitle="Filter"
-            />
-            <Scene key="single_memory_view" 
-              component={SingleMemoryView} 
-              title="Edit Memory"
-              hideTabBar={true}
-              direction="vertical"
+          <Scene key="setting_page" 
+              component={SettingsView} 
+              title="Settings Tab"
+              hideTabBar={false}
               titleStyle={{color:'black'}}
             />
-            <Scene key="filter_view" 
-              component={FilterMemoryView} 
-              title="Filter Memories"
-              hideTabBar={true}
-              direction="fade"
-              titleStyle={{color:'black'}}
+        </Scene>
+        <Scene key="tab4"  
+           title="view-list"
+
+           icon={TabIcon}>
+          <Scene key="main" 
+            component={MainView} 
+            title="Memory App"
+            onEnter={()=>console.log('onEnter')}
+            onExit={()=>console.log('onExit')}
+            onRight={() => Actions.filter_view()}
+            rightTitle="Filter"
+          />
+          <Scene key="single_memory_view" 
+            component={SingleMemoryView} 
+            title="Edit Memory"
+            hideTabBar={true}
+            direction="vertical"
+            titleStyle={{color:'black'}}
+          />
+          <Scene key="filter_view" 
+            component={FilterMemoryView} 
+            title="Filter Memories"
+            hideTabBar={true}
+            direction="fade"
+            titleStyle={{color:'black'}}
+          />
+        </Scene>
+        <Scene key="tab5"  title="note-add" icon={TabIcon}>
+          <Scene key="add_memory" 
+              component={AddMemoryView} 
+              title="Add a Memory"
             />
           </Scene>
-          <Scene key="tab5"  title="Add Memories" icon={TabIcon}>
-            <Scene key="add_memory" 
-                component={AddMemoryView} 
-                title="Add a Memory" 
-                titleStyle={{color:'black'}}
-              />
-            </Scene>
-          </Scene>
-      </Scene>
+        </Scene>
+    </Scene>
     
   
 );
@@ -150,29 +189,13 @@ export default class SouvuReactApp extends React.Component {
     return <Router createReducer={reducerCreate}  
       getSceneStyle={getSceneStyle}  
       scenes={scenes} 
-      navigationBarStyle={styles.navBar} 
-      titleStyle={styles.navBarTitle} 
-      barButtonTextStyle={styles.barButtonTextStyle} 
-      barButtonIconStyle={styles.barButtonIconStyle}
+      navigationBarStyle={nav_sytle.navBar} 
+      titleStyle={nav_sytle.navBarTitle} 
+      barButtonTextStyle={nav_sytle.barButtonTextStyle} 
+      barButtonIconStyle={nav_sytle.barButtonIconStyle}
     />;
     //return <Blink text='Niyomi!' />;
   }
 }
-
-
-styles = StyleSheet.create({
-  navBar: {
-      backgroundColor:'#0D47A1',
-  },
-  navBarTitle:{
-      color:'#FFFFFF'
-  },
-  barButtonTextStyle:{
-      color:'#FFFFFF'
-  },
-  barButtonIconStyle:{
-      tintColor:'rgb(255,255,255)'
-  },
-});
 
 AppRegistry.registerComponent('SouvuReactApp', () => SouvuReactApp);
